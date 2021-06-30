3e4ae921bf944ed9bbb959538d9e1dd3af9771f1
Thiago <thi.sanches@hotmail.com>
2021-06-30 19:14:53 -0300

    configurando banco de dados

---
+++ api/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const config = require('config');
+
+app.use(bodyParser.json());
+
+app.listen(config.get('api.porta'), () =>
+  console.log('A API esta funcionando')
+);
